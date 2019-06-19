2234f221c50dd400b4eca4a2d6f403ccc71eb8ef
Dmitriy <basharkindima@gmail.com>
2019-06-19 22:22:16 +0800

    update

---
+++ Node.js/anotherModule.js
@@ -0,0 +1,4 @@
+const data = 'some data'
+const anotherData = 'whatever'
+
+export { data, anotherData }
---
+++ Node.js/app.js
@@ -0,0 +1,5 @@
+import { data, anotherData } from './anotherModule'
+import increment from './someModule'
+
+console.log(data, anotherData)
+console.log(increment(1))
